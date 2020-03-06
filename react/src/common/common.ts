ef13d2af96f8c4fb522fd78e7c95ddbf133ee958
Oreflow <oreflow@gmail.com>
2020-03-06 22:10:05 +0000

    Moved components into folders

---
+++ react/src/common/common.ts
@@ -0,0 +1,7 @@
+import {
+  SongSection,
+} from '../../gen/proto/messages_pb';
+
+export const songSectionToString = function(songSection: SongSection): string {
+    return `${songSection.getSection()} ${songSection.getNumber()}`.trim();
+  }
