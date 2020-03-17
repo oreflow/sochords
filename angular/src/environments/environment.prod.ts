242dcb2a6e5f36b74032767c1c38fb67b7af5ad3
Tim Malmstrom <oreflow@gmail.com>
2020-03-17 16:47:37 +0000

    fixing prod-build errors

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firestorePrefix: 'PROD_',
+  firebase: {
+    apiKey: 'AIzaSyAz4cg3jwZ3fb5gwoMcxJsUZbo4piXlQQ8',
+    authDomain: 'so-chords.firebaseapp.com',
+    databaseURL: 'https://so-chords.firebaseio.com',
+    projectId: 'so-chords',
+    storageBucket: 'so-chords.appspot.com',
+    messagingSenderId: '342099760441',
+    appId: '1:342099760441:web:9d3c19f723df3b254dc4e8',
+    measurementId: 'G-E8E5FD7VGD'
+  }
 };
