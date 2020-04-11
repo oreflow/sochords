e1c6f2232b7d478f500bd6919f16dd037ded1d19
Tim Malmstrom <oreflow@gmail.com>
2020-04-11 16:03:09 +0100

    Adding lyrics block to tab section

--- angular/src/app/directives/autowidth.directive.ts
+++ angular/src/app/directives/autowidth.directive.ts
@@ -24,6 +24,13 @@ export class AutoWidthDirective implements OnInit, OnDestroy {
   }
 
   private resize() {
+    if (this.el.nativeElement.type === 'textarea') {
+      const value = this.el.nativeElement.value;
+      const sizes = value.split('\n').map((row) => row.length);
+      const cols = Math.max(this.minSize, Math.max(...sizes) + 3);
+      this.el.nativeElement.setAttribute('cols', cols);
+      return;
+    }
     const size = Math.max(this.minSize, this.el.nativeElement.value.length * 0.8);
     this.el.nativeElement.setAttribute('size', size);
   }
