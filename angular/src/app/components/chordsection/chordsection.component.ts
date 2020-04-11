4f3597cde8f6bf70c079521e488e542343f1c3e6
Tim Malmstrom <oreflow@gmail.com>
2020-04-11 11:55:29 +0100

    Cleanup and updates of chord-section

--- angular/src/app/components/chordsection/chordsection.component.ts
+++ angular/src/app/components/chordsection/chordsection.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
 import { v4 as uuidv4 } from 'uuid';
 
+import ChordService from 'app/service/chord.service';
 import { instructions } from 'gen/proto/instructions';
 import { songs } from 'gen/proto/songs';
 
@@ -16,21 +17,34 @@ const DRAGGING_ATTRIBUTE = 'dragging';
 })
 export class ChordSectionComponent implements OnInit {
   editingLyricLine: Map<string, boolean> = new Map();
-  chordsByOffset: Map<string, Map<number, instructions.IChord[]>> = new Map();
+  chordsByOffset: Map<string, Map<number, instructions.IChordInLyric[]>> = new Map();
+  chordNames: string[]; 
 
   @Input() editing: boolean;
   @Input() chordSection: songs.ChordSection;
   @Output() updated = new EventEmitter<songs.ChordSection>();
 
+  constructor(private chordService: ChordService) {}
+
   ngOnInit() {
     if (!this.chordSection.instruction) this.chordSection.instruction = {};
     if (!this.chordSection.instruction.chordsAndLyrics) this.chordSection.instruction.chordsAndLyrics = [];
-    if (!this.chordSection.instruction.chords) this.chordSection.instruction.chords= [];
-    // Tmp fix, if missing IDs for CALs, fix this.
+    if (!this.chordSection.instruction.chords) this.chordSection.instruction.chords = [];
+    // Tmp fix, if missing IDs for CALs or CILs, fix this.
     this.chordSection?.instruction?.chordsAndLyrics.forEach((cal) => {
       cal.id = cal.id || uuidv4();
+      if (!cal.chordsInLyric) cal.chordsInLyric = [];
+      cal.chordsInLyric.forEach((cil) => {
+        cil.id = cil.id || uuidv4();
+      });
     });
     this._updateChordsByOffset();
+    this._loadChordNames();
+  }
+
+  async _loadChordNames() {
+    const chordNames = await this.chordService.getChordNames();
+    this.chordNames = Array.from(new Set(chordNames));
   }
 
   addChordsAndLyrics() {
@@ -61,14 +75,12 @@ export class ChordSectionComponent implements OnInit {
     $event.dataTransfer.setData('chord', JSON.stringify(chord));
   }
 
-  existingChordDragStart($event, calId: string, charIndex: number, chord: instructions.Chord) {
+  existingChordDragStart($event, calId: string, cilId: string) {
     $event.dataTransfer.setData('calId', calId);
-    $event.dataTransfer.setData('charIndex', charIndex);
-    $event.dataTransfer.setData('chord', JSON.stringify(chord));
+    $event.dataTransfer.setData('cilId', cilId);
   }
 
   setDragOverAttr($event) {
-    console.log('dragover', $event);
     $event.target.setAttribute(DRAGGING_ATTRIBUTE, '');
     $event.preventDefault();
   }
@@ -78,10 +90,8 @@ export class ChordSectionComponent implements OnInit {
     $event.preventDefault();
   }
 
-  removeInlineChord(cal: instructions.ChordsAndLyrics, charIndex: number, chord: instructions.Chord) {
-    const cil = cal.chordsInLyric
-      .find(cil => cil.chord === chord && cil.offset === charIndex);
-    cal.chordsInLyric.splice(cal.chordsInLyric.indexOf(cil), 1);
+  removeInlineChord(cal: instructions.ChordsAndLyrics, cilId: string) {
+    cal.chordsInLyric = cal.chordsInLyric.filter(cil => cil.id !== cilId);
     this._updateChordsByOffset();
     this.onUpdate();
   }
@@ -89,20 +99,20 @@ export class ChordSectionComponent implements OnInit {
   droppedOnCharacter($event, cal: instructions.ChordsAndLyrics, offset: number) {
     $event.target.removeAttribute(DRAGGING_ATTRIBUTE, '');
     if ($event.dataTransfer.getData('calId')) {
+      // The chord was dragged to be moved.
       const calId: string = $event.dataTransfer.getData('calId');
-      const charIndex: number = parseInt($event.dataTransfer.getData('charIndex'), 10);
-      const chordString = $event.dataTransfer.getData('chord');
+      const cilId: string = $event.dataTransfer.getData('cilId');
 
       const dragCal = this.chordSection.instruction.chordsAndLyrics.find((cal) => cal.id === calId);
-      const dragCil = dragCal.chordsInLyric
-        .find(cil => JSON.stringify(cil.chord) === chordString && cil.offset === charIndex);
+      const dragCil = dragCal.chordsInLyric.find(cil => cil.id === cilId);
 
-      dragCal.chordsInLyric.splice(cal.chordsInLyric.indexOf(dragCil), 1);
-      cal.chordsInLyric.push({ offset, chord: dragCil.chord });
+      dragCal.chordsInLyric = dragCal.chordsInLyric.filter((cil) => cil.id !== cilId);
+      cal.chordsInLyric.push({ offset, chord: dragCil.chord, id: uuidv4() });
     } else {
+      // The chord was to create new.
       const chord: instructions.Chord = JSON.parse($event.dataTransfer.getData('chord'));
       if (!cal.chordsInLyric) cal.chordsInLyric = [];
-      cal.chordsInLyric.push({ offset, chord });
+      cal.chordsInLyric.push({ offset, chord, id: uuidv4() });
     }
     this._updateChordsByOffset();
     this.onUpdate();
@@ -123,9 +133,9 @@ export class ChordSectionComponent implements OnInit {
   getAdjustedLyricLine(cal: instructions.ChordsAndLyrics) {
     // Pads lyric with 10 spaces on each side for extra chord-space.
     return [
-      ...new Array(10).fill(' '),
+      ...new Array(10).fill(''),
       ...(cal?.lyricLine?.split('') || []),
-      ...new Array(10).fill(' '),
+      ...new Array(10).fill(''),
     ];
   }
 
@@ -137,13 +147,12 @@ export class ChordSectionComponent implements OnInit {
       cal.chordsInLyric.forEach((cil) => {
         if (!this.chordsByOffset.get(cal.id).has(cil.offset))
           this.chordsByOffset.get(cal.id).set(cil.offset, []);
-        this.chordsByOffset.get(cal.id).get(cil.offset).push(cil.chord);
+        this.chordsByOffset.get(cal.id).get(cil.offset).push(cil);
       });
     });
   }
 
   onUpdate() {
-    this.chordSection.instruction.chordsAndLyrics.forEach((cal) => console.log(cal.lyricLine));
     if (!this.editing) return;
     this.updated.emit(this.chordSection);
   }
